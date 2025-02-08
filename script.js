436b10161db5012b7b1d4166d3f32c1e91ceb241
sapanagupta25 <146055257+sapanagupta25@users.noreply.github.com>
2025-02-08 21:23:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	function indexOfIgnoreCase(s1, s2) {
-  if (!s2) return 0; // Edge case: empty subStr should return 0 (similar to indexOf behavior)
-  
-  return s1.toLowerCase().indexOf(s2.toLowerCase());
+	  return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
 
-}
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
